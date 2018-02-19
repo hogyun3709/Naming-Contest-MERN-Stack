548db59425bb938526d30e98e2d68b40248e00a6
hogyun3709 <hogyu3709@gmail.com>
2018-02-20 02:24:26 +0900

    Change React 16+ to 15+: Server side rendering-initial render markup

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import config from './config';
-import express from 'express';
 import apiRouter from './api';
 import sassMiddleware from 'node-sass-middleware';
 import path from 'path';
-import './serverRender';
-// import fs from 'fs';
 
+import express from 'express';
 const server = express();
 
 server.use(sassMiddleware({
@@ -14,23 +12,23 @@ server.use(sassMiddleware({
 }));
 
 server.set('view engine', 'ejs');
+
+import serverRender from './serverRender';
+
 server.get('/', (req, res) => {
-  res.render('index', {
-    content: '...'
-  });
+  serverRender()
+    .then(( {initialMarkup, initialData}) => {
+      res.render('index', {
+        initialMarkup,
+        initialData
+      });
+    })
+    .catch(console.error);
 });
 
-//For developemt purpose using middle ware
-server.use(express.static('public'));
 server.use('/api', apiRouter);
-// server.get('/about.html', (req, res) => {
-//   fs.readFile('./about.html', (err, data) => {
-//     res.send(data.toString());
-//   });
-// });
-server.get('/profile.html', (req,res) => {
-  res.send('Profile');
-});
+server.use(express.static('public'));
+
 server.listen(config.port, config.host, () => {
-  console.info('Express listening on port ', config.port);
+  console.info('Express listening on port', config.port);
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
   state = {
     pageHeader: 'Naming Contests',
-    contests:[]
+    contests: this.props.initialContests
   };
   componentDidMount() {
-    // ajax request
-    axios.get('/api/contests')
-      .then(resp => {
-        this.setState({
-          contests: resp.data.contests
-        });
-      })
-      .catch(console.error);
+
   }
   componentWillUnmount() {
+    // clean timers, listeners
   }
   render() {
     return (
@@ -26,7 +19,7 @@ class App extends React.Component {
         <Header message={this.state.pageHeader} />
         <div>
           {this.state.contests.map(contest =>
-            <ContestPreview key={contest.id}{...contest}/>
+            <ContestPreview key={contest.id} {...contest} />
           )}
         </div>
       </div>
