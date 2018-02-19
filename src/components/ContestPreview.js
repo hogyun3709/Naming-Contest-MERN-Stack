7cce2ef7a760150c8423003734672d0232841da1
hogyun3709 <hogyu3709@gmail.com>
2018-02-19 23:29:47 +0900

    Using Sass with node middleware

--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types'
 const ContestPreview = (contest) => {
   return (
     <div className="ContestPreview">
-      <div>
+      <div className="category-name">
         {contest.categoryName}
       </div>
-      <div>
+      <div className="contest-name">
         {contest.contestName}
       </div>
     </div>
