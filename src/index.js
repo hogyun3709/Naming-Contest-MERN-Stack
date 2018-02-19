31a87ed7c4a3d11ca77a2bef98bcffcde1d1647e
hogyun3709 <hogyu3709@gmail.com>
2018-02-19 16:56:24 +0900

    React components composability

--- src/index.js
+++ src/index.js
@@ -2,21 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-const App = (props) => {
-  return (
-    <h2 className="text-center">
-      {props.headerMessage}
+const Header = ({ message }) => {
+  return(
+    <h2 className="Header text-center">
+      {message}
     </h2>
   );
 };
 
-App.propTypes = {
-  headerMessage: PropTypes.string
+Header.propTypes = {
+  message: PropTypes.string
 };
 
-App.defaultProps ={
-  headerMessage: 'Hello World'
+const App = () => {
+  return(
+    <div className="App">
+      <Header message="Naming contest"/>
+      <div>
+        ..
+      </div>
+    </div>
+
+  );
 };
+
+
+
+
 
 
 ReactDOM.render(
