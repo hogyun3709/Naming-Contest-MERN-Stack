59d05c5ab36bffddcfd81ef8227e2e9eabe2f075
hogyun3709 <hogyu3709@gmail.com>
2018-02-20 12:45:20 +0900

    Handling the contest with click event

---
+++ src/components/ContestList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ContestPreview from './ContestPreview';
+
+
+const ContestList = ({ contests }) => {
+  return (
+
+    <div>
+      {contests.map(contest =>
+        <ContestPreview key={contest.id} {...contest} />
+      )}
+    </div>
+  );
+};
+ContestList.propTypes = {
+  contests: React.PropTypes.array
+};
+
+export default ContestList;
