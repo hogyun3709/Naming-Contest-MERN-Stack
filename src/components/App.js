653cf1761608b2febe61fa7a50a75208776775e0
hogyun3709 <hogyu3709@gmail.com>
2018-03-08 17:29:10 +0900

    Navigating to a list of contest

--- src/components/App.js
+++ src/components/App.js
@@ -35,6 +35,18 @@ class App extends React.Component {
       });
     });
   };
+  fetchContestList = () => {
+    pushState(
+      { currentContestId: null},
+      `/`
+    );
+    api.fetchContestList().then(contests => {
+      this.setState({
+        currentContestId: null,
+        contests
+      });
+    });
+  };
   pageHeader(){
     if(this.state.currentContestId){
       return this.currentContest().contestName;
@@ -47,7 +59,9 @@ class App extends React.Component {
   }
   currentContent(){
     if (this.state.currentContestId){
-      return <Contest {...this.currentContest()}/>;
+      return <Contest
+        contestListClick={this.fetchContestList}
+        {...this.currentContest()}/>;
     } else {
       return <ContestList
         onContestClick={this.fetchContest}
